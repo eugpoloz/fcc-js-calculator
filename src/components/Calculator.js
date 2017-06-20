2324a453f58c36d9ac69ce9b5469c66dbdc63f6c
Evgenia Polozova <eugpoloz@gmail.com>
2017-06-21 00:52:45 +0300

    create logic for adding numbers to textarea and such

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,105 +9,136 @@ import {
   LabelHidden
 } from './StyledCalculator';
 
-const buttonConfig = [
-  {
-    key: 'C',
-    action: 'clear-all'
-  },
-  {
-    key: 'CE',
-    action: 'clear'
-  },
-  {
-    key: '∓',
-    action: 'neg-pos'
-  },
-  {
-    key: '÷',
-    action: '/'
-  },
-  {
-    key: 7,
-    action: 7
-  },
-  {
-    key: 8,
-    action: 8
-  },
-  {
-    key: 9,
-    action: 9
-  },
-  {
-    key: '*',
-    action: '*'
-  },
-  {
-    key: 4,
-    action: 4
-  },
-  {
-    key: 5,
-    action: 5
-  },
-  {
-    key: 6,
-    action: 6
-  },
-  {
-    key: '-',
-    action: '-'
-  },
-  {
-    key: 1,
-    action: 1
-  },
-  {
-    key: 2,
-    action: 2
-  },
-  {
-    key: 3,
-    action: 3
-  },
-  {
-    key: '+',
-    action: '+'
-  },
-  {
-    key: '.',
-    action: '.'
-  },
-  {
-    key: 0,
-    action: 0
-  },
-  {
-    key: '%',
-    action: '%'
-  },
-  {
-    key: '=',
-    action: '='
-  }
-];
-
 class Calculator extends Component {
   state = {
     equation: 0,
     value: 0
   };
 
-  handleChange = (e) => {
-    const value = e.target.value !== '' ? e.target.value : 0;
+  updateInput = (e) => {
+    let value;
+
+    if (e.target.value) {
+      value = e.target.value;
+    } else if (e.key) {
+      value = '' + this.state.value + e.key;
+    }
 
     this.setState({
-      equation: value,
-      value
+      value: isNaN(+value) ? 0 : +value
     })
   };
 
+  clearAll = (e) => {
+    this.setState({
+      equation: 0,
+      value: 0
+    })
+  };
+
+  clearEntry = (e) => {
+    this.setState({
+      value: 0
+    })
+  };
+
+  updateValue = (e) => {
+    if (e.keyCode > 47 && e.keyCode < 58) {
+      this.updateInput(e);
+    }
+    if (e.keyCode === 67) {
+      this.clearAll(e);
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('keyup', this.updateValue);
+  }
+
   render() {
+    const buttonConfig = [
+      {
+        key: 'C',
+        action: this.clearAll
+      },
+      {
+        key: 'CE',
+        action: this.clearEntry
+      },
+      {
+        key: '∓',
+        // action: 'neg-pos'
+      },
+      {
+        key: '÷',
+        // action: '/'
+      },
+      {
+        key: 7,
+        // action: 7
+      },
+      {
+        key: 8,
+        // action: 8
+      },
+      {
+        key: 9,
+        // action: 9
+      },
+      {
+        key: '*',
+        // action: '*'
+      },
+      {
+        key: 4,
+        // action: 4
+      },
+      {
+        key: 5,
+        // action: 5
+      },
+      {
+        key: 6,
+        // action: 6
+      },
+      {
+        key: '-',
+        // action: '-'
+      },
+      {
+        key: 1,
+        // action: 1
+      },
+      {
+        key: 2,
+        // action: 2
+      },
+      {
+        key: 3,
+        // action: 3
+      },
+      {
+        key: '+',
+        // action: '+'
+      },
+      {
+        key: '.',
+        // action: '.'
+      },
+      {
+        key: 0,
+        // action: 0
+      },
+      {
+        key: '%',
+        // action: '%'
+      },
+      {
+        key: '=',
+        // action: '='
+      }
+    ];
+
     return (
       <Container>
         <ResultContainer>
@@ -118,12 +149,12 @@ class Calculator extends Component {
           <ResultInput
             name="result"
             value={this.state.value}
-            onChange={this.handleChange}
+            onChange={this.updateInput}
           />
         </ResultContainer>
         <ButtonContainer>
           {buttonConfig.map(btn =>
-            <Button key={btn.action}>
+            <Button key={btn.key} onClick={btn.action ? btn.action : false}>
               {btn.key}
             </Button>
           )}
